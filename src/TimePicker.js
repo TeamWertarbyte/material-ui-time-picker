dc1463c2c0cc2bc81db5768e04b7cbc7fdeba3d1
leMaik <an@maik.tk>
2017-10-15 02:14:48 +0200

    Add the box around the time picker.

--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
+import { duration, easing } from 'material-ui/styles/transitions'
+import { getContrastRatio } from 'material-ui/styles/colorManipulator'
+import classNames from 'classnames'
 import Clock from './Clock'
 
-const styles = {
-
-}
+const styles = (theme) => ({
+  root: {
+    width: 280
+  },
+  header: {
+    background: theme.palette.primary[500],
+    color: getContrastRatio(theme.palette.primary[500], theme.palette.common.lightBlack) < 7 ? theme.palette.common.lightWhite : theme.palette.common.lightBlack,
+    padding: '20px 0',
+    lineHeight: '58px',
+    height: 58,
+    fontSize: '58px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline'
+  },
+  time: {
+    transition: `all ${duration.short}ms ${easing.easeInOut}`,
+    cursor: 'pointer'
+  },
+  placeholder: {
+    flex: 1
+  },
+  ampm: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    flex: 1,
+    fontSize: '14px',
+    lineHeight: '20px',
+    marginLeft: 16,
+    fontWeight: 700
+  },
+  select: {
+    color: getContrastRatio(theme.palette.primary[500], theme.palette.common.fullBlack) < 7 ? theme.palette.common.fullWhite : theme.palette.common.fullBlack
+  },
+  body: {
+    padding: '24px 16px',
+    background: theme.palette.background.paper
+  }
+})
 
 class TimePicker extends React.Component {
   constructor (props) {
@@ -30,46 +69,106 @@ class TimePicker extends React.Component {
 
   handleClockChange = (value) => {
     if (this.state.select === 'h') {
-      this.setState({ hours: value })
+      if (this.props.mode === '12h' && this.state.hours >= 12) {
+        this.setState({ hours: value + 12 }, this.propagateChange)
+      } else {
+        this.setState({ hours: value === 12 ? 0 : value }, this.propagateChange)
+      }
     } else {
-      this.setState({ minutes: value })
+      this.setState({ minutes: value }, this.propagateChange)
     }
   }
 
   handleClockChangeDone = (e) => {
     e.preventDefault() // prevent mouseUp after touchEnd
 
-    setImmediate(() => {
-      if (this.state.select === 'h') {
-        this.setState({ select: 'm' })
-      } else if (this.props.onChange != null) {
-        const date = new Date()
-        date.setHours(this.state.hours)
-        date.setMinutes(this.state.minutes)
-        date.setSeconds(0)
-        date.setMilliseconds(0)
-        this.props.onChange(date)
-      }
-    })
+    setTimeout(() => {
+      this.setState({ select: 'm' })
+    }, 300)
+  }
+
+  editHours = () => this.setState({ select: 'h' })
+
+  editMinutes = () => this.setState({ select: 'm' })
+
+  setAm = () => {
+    if (this.state.hours >= 12) {
+      this.setState({ hours: this.state.hours - 12 }, this.propagateChange)
+    }
+  }
+
+  setPm = () => {
+    if (this.state.hours < 12) {
+      this.setState({ hours: this.state.hours + 12 }, this.propagateChange)
+    }
+  }
+
+  propagateChange = () => {
+    if (this.props.onChange != null) {
+      const date = new Date()
+      date.setHours(this.state.hours)
+      date.setMinutes(this.state.minutes)
+      date.setSeconds(0)
+      date.setMilliseconds(0)
+      this.props.onChange(date)
+    }
   }
 
   render () {
     const {
+      classes,
       mode
     } = this.props
 
     const clockMode = this.state.select === 'm' ? 'minutes' : mode
+    const { minutes } = this.state
     const { hours, isPm } = convert12HourClock(this.state.hours, mode)
 
     return (
-      <div>
-        <Clock
-          mode={clockMode}
-          onChange={this.handleClockChange}
-          value={clockMode === 'minutes' ? this.state.minutes : hours}
-          onMouseUp={this.handleClockChangeDone}
-          onTouchEnd={this.handleClockChangeDone}
-        />
+      <div className={classes.root}>
+        <div className={classes.header}>
+          <div className={classes.placeholder} />
+          <div>
+            <span
+              className={classNames(classes.time, { [classes.select]: this.state.select === 'h' && 'active' })}
+              onClick={this.editHours}
+            >
+              {hours < 10 ? `0${hours}` : hours}
+            </span>
+            :
+            <span
+              className={classNames(classes.time, { [classes.select]: this.state.select === 'm' && 'active' })}
+              onClick={this.editMinutes}
+            >
+              {minutes < 10 ? `0${minutes}` : minutes}
+            </span>
+          </div>
+          {mode === '12h' ? (
+            <div className={classes.ampm}>
+              <span
+                className={classNames(classes.time, { [classes.select]: isPm })}
+                onClick={this.setPm}
+              >
+                PM
+              </span>
+              <span
+                className={classNames(classes.time, { [classes.select]: !isPm })}
+                onClick={this.setAm}
+              >
+                AM
+              </span>
+            </div>
+          ) : (<div className={classes.placeholder} />)}
+        </div>
+        <div className={classes.body}>
+          <Clock
+            mode={clockMode}
+            onChange={this.handleClockChange}
+            value={clockMode === 'minutes' ? minutes : hours}
+            onMouseUp={this.handleClockChangeDone}
+            onTouchEnd={this.handleClockChangeDone}
+          />
+        </div>
       </div>
     )
   }
@@ -87,7 +186,7 @@ function convert12HourClock (hours, mode) {
   const isPm = hours >= 12
   if (mode === '24h') {
     return { hours, isPm }
-  } else if (hours === 0) {
+  } else if (hours === 0 || hours === 12) {
     return { hours: 12, isPm }
   } else if (hours < 12) {
     return { hours, isPm }
---
+++ src/TimePicker.spec.js
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+import React from 'react'
+import { mount } from 'enzyme'
+import * as testUtils from '../test/utils'
+import TimePicker from './TimePicker'
+
+describe('<TimePicker />', () => {
+
+})
