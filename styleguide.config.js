1167f0bb9dedfc75c58e0a847d640b44486fbc22
Maik Marschner <an@maik.tk>
2017-10-18 12:33:38 +0200

    Improve styleguide config to show proper import lines.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,8 +1,13 @@
 const { createConfig, babel } = require('webpack-blocks')
+const path = require('path')
 
 module.exports = {
   components: 'src/**/[A-Z]*.js',
   webpackConfig: createConfig([
     babel()
-  ])
+  ]),
+  getComponentPathLine: (componentPath) => {
+    const name = path.basename(componentPath, '.js')
+    return `import { ${name} } from 'material-ui-time-picker';`
+  }
 }
