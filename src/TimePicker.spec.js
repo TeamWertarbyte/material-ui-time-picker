aabcbafe2ac66704c63f15e55425362bc3ac2d54
Serg <Sergio.357.95@gmail.com>
2018-08-20 16:33:58 +0200

    Updated snapshots and test specs

--- src/TimePicker.spec.js
+++ src/TimePicker.spec.js
@@ -78,7 +78,6 @@ describe('<TimePicker />', () => {
         .simulate('click', testUtils.stubClickEvent(190, 230)) // click on 25
         .simulate('mouseup', testUtils.stubClickEvent(190, 230)) // click on 25
 
-      expect(changeHandler).toBeCalled()
       expect(changeHandler.mock.calls[0][0].getHours()).toBe(12)
       expect(changeHandler.mock.calls[0][0].getMinutes()).toBe(25)
     })
@@ -260,7 +259,6 @@ describe('<TimePicker />', () => {
         .simulate('click', testUtils.stubClickEvent(190, 230)) // click on 25
         .simulate('mouseup', testUtils.stubClickEvent(190, 230)) // click on 25
 
-      expect(changeHandler).toBeCalled()
       expect(changeHandler.mock.calls[0][0].getHours()).toBe(12)
       expect(changeHandler.mock.calls[0][0].getMinutes()).toBe(25)
     })
